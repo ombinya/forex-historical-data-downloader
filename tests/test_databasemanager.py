55dab2e37f9cd94473cc25a8c72f7fe3fc7ad0bd
Paul Muoga <paul.sofdev@gmail.com>
2024-08-07 11:50:11 +0300

    Added test for create_table

--- tests/test_databasemanager.py
+++ tests/test_databasemanager.py
@@ -1,9 +1,29 @@
 import unittest
 import sys
 import os
+from datetime import datetime
 
 # Add the parent directory to the sys.path
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 
 from app.databasemanager import DatabaseManager
 
+class TestDatabaseManager(unittest.IsolatedAsyncioTestCase):
+    def setUp(self):
+        self.asset = "asset"
+
+        datetimeformat = "%Y_%m_%d_%H_%M_%S"
+        currentdatetime = datetime.now()
+        datetimestring = currentdatetime.strftime(datetimeformat)
+        self.dbfilename = self.asset + "_" + datetimestring + ".db"
+        self.databasemanager = DatabaseManager(self.dbfilename, self.asset)
+
+    async def test_create_table(self):
+        await self.databasemanager.create_table()
+        dbfilepath = self.databasemanager.get_downloads_folder_path() + "/" + self.dbfilename
+
+        self.assertTrue(os.path.exists(dbfilepath))
+
+    def tearDown(self):
+        # Delete database file after test
+        os.remove(self.databasemanager.dbfilepath)
